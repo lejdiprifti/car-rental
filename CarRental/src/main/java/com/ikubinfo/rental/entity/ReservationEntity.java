650837af027e37b066ed370222ad632c3936d8f8
lejdiprifti <priftilejdi@gmail.com>
2020-02-18 10:36:29 +0100

    added fetch type

--- CarRental/src/main/java/com/ikubinfo/rental/entity/ReservationEntity.java
+++ CarRental/src/main/java/com/ikubinfo/rental/entity/ReservationEntity.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +23,7 @@ public class ReservationEntity {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private UserEntity user;
 
--- CarRental/src/main/java/com/ikubinfo/rental/entity/UserEntity.java
+++ CarRental/src/main/java/com/ikubinfo/rental/entity/UserEntity.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -48,7 +49,7 @@ public class UserEntity {
 	@Column(name="email")
 	private String email;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="role_id")
 	private RoleEntity role;
 	
