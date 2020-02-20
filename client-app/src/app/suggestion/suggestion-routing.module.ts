1aa3660d9411fbfa3387cee6e083eb78ab59e9f3
lejdiprifti <priftilejdi@gmail.com>
2020-02-20 17:35:35 +0100

    deleted category component

--- client-app/src/app/suggestion/suggestion-routing.module.ts
+++ client-app/src/app/suggestion/suggestion-routing.module.ts
@@ -1,45 +1,54 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AdminGuard } from '@ikubinfo/core/guards/admin-guard';
-import { DashboardComponent } from '@ikubinfo/suggestion/dashboard/dashboard.component';
-import { FullComponent } from '@ikubinfo/layout/full/full.component';
-import { UserComponent } from '@ikubinfo/suggestion/user/user.component';
-import { UserGuard } from '@ikubinfo/core/guards/user-guard';
-import { CategoryComponent } from '@ikubinfo/suggestion/category/category.component';
-import { CategoriesComponent } from '@ikubinfo/suggestion/categories/categories.component';
-import { CarsComponent } from '@ikubinfo/suggestion/cars/cars.component';
-import { CarComponent } from '@ikubinfo/suggestion/car/car.component';
-import { BookingComponent } from '@ikubinfo/suggestion/booking/booking.component';
-import { BookingsComponent } from '@ikubinfo/suggestion/bookings/bookings.component';
-import { ClientsComponent } from '@ikubinfo/suggestion/clients/clients.component';
+import { AdminGuard } from "@ikubinfo/core/guards/admin-guard";
+import { DashboardComponent } from "@ikubinfo/suggestion/dashboard/dashboard.component";
+import { FullComponent } from "@ikubinfo/layout/full/full.component";
+import { UserComponent } from "@ikubinfo/suggestion/user/user.component";
+import { UserGuard } from "@ikubinfo/core/guards/user-guard";
+import { CategoriesComponent } from "@ikubinfo/suggestion/categories/categories.component";
+import { CarsComponent } from "@ikubinfo/suggestion/cars/cars.component";
+import { CarComponent } from "@ikubinfo/suggestion/car/car.component";
+import { BookingComponent } from "@ikubinfo/suggestion/booking/booking.component";
+import { BookingsComponent } from "@ikubinfo/suggestion/bookings/bookings.component";
+import { ClientsComponent } from "@ikubinfo/suggestion/clients/clients.component";
 
 const suggestionRoutes: Routes = [
-    {
-        path: '',
-        component: FullComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
-            { path: 'user', component: UserComponent, canActivate: [UserGuard] },
-            { path: 'categories', component: CategoriesComponent, canActivate: [AdminGuard] },
-            { path: 'category', component: CategoryComponent, canActivate: [AdminGuard] },
-            { path: 'category/:id', component: CategoryComponent, canActivate: [AdminGuard] },
-            { path: 'cars', component: CarsComponent },
-            { path: 'car/:id', component: CarComponent, canActivate: [AdminGuard] },
-            { path: 'car', component: CarComponent, canActivate: [AdminGuard] },
-            { path: 'reservations', component: BookingsComponent },
-            { path: 'clients', component: ClientsComponent, canActivate: [AdminGuard] },
-            { path: 'reservation/:carId', component: BookingComponent, canActivate: [UserGuard] }
-        ]
-    }
+  {
+    path: "",
+    component: FullComponent,
+    children: [
+      {
+        path: "dashboard",
+        component: DashboardComponent,
+        canActivate: [AdminGuard]
+      },
+      { path: "user", component: UserComponent, canActivate: [UserGuard] },
+      {
+        path: "categories",
+        component: CategoriesComponent,
+        canActivate: [AdminGuard]
+      },
+      { path: "cars", component: CarsComponent },
+      { path: "car/:id", component: CarComponent, canActivate: [AdminGuard] },
+      { path: "car", component: CarComponent, canActivate: [AdminGuard] },
+      { path: "reservations", component: BookingsComponent },
+      {
+        path: "clients",
+        component: ClientsComponent,
+        canActivate: [AdminGuard]
+      },
+      {
+        path: "reservation/:carId",
+        component: BookingComponent,
+        canActivate: [UserGuard]
+      }
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forChild(suggestionRoutes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [RouterModule.forChild(suggestionRoutes)],
+  exports: [RouterModule]
 })
-export class SuggestionRoutingModule { }
+export class SuggestionRoutingModule {}
--- client-app/src/app/suggestion/suggestion.module.ts
+++ client-app/src/app/suggestion/suggestion.module.ts
@@ -1,38 +1,69 @@
-import { CommonsModule } from '@ikubinfo/commons/commons.module';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { CommonsModule } from "@ikubinfo/commons/commons.module";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { FullCalendarModule } from "@fullcalendar/angular";
 
-import { DashboardComponent } from '@ikubinfo/suggestion/dashboard/dashboard.component';
-import { SuggestionRoutingModule } from '@ikubinfo/suggestion/suggestion-routing.module';
-import { LayoutModule } from '@ikubinfo/layout/layout.module';
-import { UserComponent } from '@ikubinfo/suggestion/user/user.component';
-import { CategoryComponent } from '@ikubinfo/suggestion/category/category.component';
-import { CategoriesComponent } from '@ikubinfo/suggestion/categories/categories.component';
-import { CarsComponent } from '@ikubinfo/suggestion/cars/cars.component';
-import { CarComponent } from '@ikubinfo/suggestion/car/car.component';
-import { BookingComponent } from '@ikubinfo/suggestion/booking/booking.component';
-import { BookingsComponent } from '@ikubinfo/suggestion/bookings/bookings.component';
-import { ClientsComponent } from '@ikubinfo/suggestion/clients/clients.component';
+import { DashboardComponent } from "@ikubinfo/suggestion/dashboard/dashboard.component";
+import { SuggestionRoutingModule } from "@ikubinfo/suggestion/suggestion-routing.module";
+import { LayoutModule } from "@ikubinfo/layout/layout.module";
+import { UserComponent } from "@ikubinfo/suggestion/user/user.component";
+import { CategoriesComponent } from "@ikubinfo/suggestion/categories/categories.component";
+import { CarsComponent } from "@ikubinfo/suggestion/cars/cars.component";
+import { CarComponent } from "@ikubinfo/suggestion/car/car.component";
+import { BookingComponent } from "@ikubinfo/suggestion/booking/booking.component";
+import { BookingsComponent } from "@ikubinfo/suggestion/bookings/bookings.component";
+import { ClientsComponent } from "@ikubinfo/suggestion/clients/clients.component";
 
-import { DataViewModule } from 'primeng/dataview';
+import { DataViewModule } from "primeng/dataview";
 import {
-    PasswordModule, FileUploadModule, CardModule, ToggleButtonModule, SelectButtonModule,
-    ProgressSpinnerModule, DialogModule, SplitButtonModule, PanelModule, InplaceModule,
-    KeyFilterModule, DropdownModule
-} from 'primeng/primeng';
-
-
+  PasswordModule,
+  FileUploadModule,
+  CardModule,
+  ToggleButtonModule,
+  SelectButtonModule,
+  ProgressSpinnerModule,
+  DialogModule,
+  SplitButtonModule,
+  PanelModule,
+  InplaceModule,
+  KeyFilterModule,
+  DropdownModule
+} from "primeng/primeng";
 
 @NgModule({
-    imports: [CommonsModule, DialogModule, SelectButtonModule, SplitButtonModule, FullCalendarModule,
-        ProgressSpinnerModule, DropdownModule, ToggleButtonModule, KeyFilterModule, InplaceModule, PanelModule,
-        ToggleButtonModule, SuggestionRoutingModule, DataViewModule, LayoutModule, CardModule, FormsModule, PasswordModule,
-        FileUploadModule],
-    exports: [],
-    declarations: [DashboardComponent, BookingsComponent, BookingComponent, CarComponent, ClientsComponent,
-        CarsComponent, UserComponent, CategoryComponent, CategoriesComponent],
-    providers: []
+  imports: [
+    CommonsModule,
+    DialogModule,
+    SelectButtonModule,
+    SplitButtonModule,
+    FullCalendarModule,
+    ProgressSpinnerModule,
+    DropdownModule,
+    ToggleButtonModule,
+    KeyFilterModule,
+    InplaceModule,
+    PanelModule,
+    ToggleButtonModule,
+    SuggestionRoutingModule,
+    DataViewModule,
+    LayoutModule,
+    CardModule,
+    FormsModule,
+    PasswordModule,
+    FileUploadModule
+  ],
+  exports: [],
+  declarations: [
+    DashboardComponent,
+    BookingsComponent,
+    BookingComponent,
+    CarComponent,
+    ClientsComponent,
+    CarsComponent,
+    UserComponent,
+    CategoriesComponent
+  ],
+  providers: []
 })
-export class SuggestionModule { }
+export class SuggestionModule {}
